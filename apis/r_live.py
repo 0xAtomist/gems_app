c819da4ec1d72a3353e11953d4f3b32a1100dac8
Tom Berry <berry.tom@live.co.uk>
2021-05-15 22:50:56 +0000

    v0.0.3 - styling and sidebar prices

--- apis/r_live.py
+++ apis/r_live.py
@@ -10,18 +10,16 @@ df = get_api_markets()
 
 r = redis.StrictRedis('localhost', charset='utf-8', decode_responses=True)
 
-print(df.loc['bitcoin'])
-
 for key in df.index:
-	print(key)
+#	print(key)
 	r_dict = df.loc[key].to_dict()
-	print(r_dict)
+#	print(r_dict)
 	r.hmset(key, r_dict)
 
 output = r.hgetall('mainframe')
 #decoded = {key.decode('utf-8'):value.decode('utf-8') for key, value in output.items()}
 print(output)
 df = pd.DataFrame.from_dict(output, orient='index')
-print(df)
+#print(df)
 
 
