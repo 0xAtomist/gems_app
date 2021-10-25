af235cac988c82cc61a581ca4d3aebe25bdf95b8
Atomist <32900274+0xAtomist@users.noreply.github.com>
2021-10-25 22:58:17 +0100

    Update gmx_staking_recent.py

--- apis/gmx_staking_recent.py
+++ apis/gmx_staking_recent.py
@@ -17,21 +17,22 @@ ETH_USDC_LP = '0x17c14d2c404d167802b16c450d3c99f88f2c4f4d'
 API_KEY = auth_conf['arbiscan']['API_KEY']
 
 
-def get_staked_recent(df_all, n_total):
+def get_staked_recent():
     r_100 = requests.get('https://api.arbiscan.io/api?module=account&action=tokentx&contractaddress={}&address={}&page=1&offset=100&sort=desc&apikey={}'
                         .format(GMX_contract, sGMX_contract, API_key))
 
     df = pd.DataFrame(columns=['value', 'timestamp', 'in/out'])
 
-    output_dict = {
-        'value': [],
-        'cum_value': [],
-        'timestamp': [],
-        'in/out': []
-    }
+    output_dict = {}
 
     
     for result in r_100.json()['result']:
+        tx = result['hash']
+        timestamp = result['timeStamp']
+        blocknumber = result['blockNumber']
+        
+        output_dict[tx] = {'timestamp': timestamp, 'blocknumber': blocknumber, 'tx_hash': tx}
+        
         if result['to'] == '0x908c4d94d34924765f1edc22a1dd098397c59dd4':
             value = float(float(result['value'])*1e-18)
             in_out = 'IN'
@@ -42,41 +43,21 @@ def get_staked_recent(df_all, n_total):
         
         cum_value = 0
 
-        output_dict['value'].append(value)
-        output_dict['cum_value'].append(cum_value)
-        output_dict['timestamp'].append(timestamp)
-        output_dict['in/out'].append(in_out)
+        output_dict[tx]['value'].append(value)
+        output_dict[tx]['cum_value'].append(cum_value)
+        output_dict[tx]['timestamp'].append(timestamp)
+        output_dict[tx]['in/out'].append(in_out)
 
 
     df = pd.DataFrame.from_dict(output_dict) 
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
 
-    wait_list = []
-    
-    for i, timestamp in enumerate(df['timestamp']):
-        if timestamp in df_all['timestamp']:
-            print('stored')
-            df = df.drop([i])
-            pass
-        else:
-            print('new', i)
-            wait_list.append(i)
-
-    for i in wait_list:
-        if i == wait_list[0]:
-            prev = df_all['cum_value'].iloc[-1]
-            print(prev)
-        else:
-            prev = df['cum_value'].iloc[i-1]
-            print(prev)
-        cum_value = prev + df['value'].iloc[i]
-        df['cum_value'].iloc[i] = cum_value
-
-    df = df.dropna().drop_duplicates(subset=['timestamp']).set_index('timestamp', drop=False)
-
-    df = df_all.append(df)
-    df['Staked %'] = df['cum_value']/n_total*100
-
+    df = df.dropna().set_index('timestamp', drop=False)
+    dt = pd.to_datetime(df['timestamp'], format='%Y/%m/%d %H:%M:%S')
+    delta = pd.to_timedelta(df.groupby(dt).cumcount(), unit='ms')
+    df['timestamp'] = dt + delta.values
+    df = df.dropna().set_index('timestamp', drop=False)
+    df = df.sort_index()
 
     return df
 
@@ -100,21 +81,26 @@ context = pa.default_serialization_context()
 
 while True:
     try:
+        df_all = context.deserialize(r.get('gmx-staked'))
+        df_100 = get_staked_recent()
+
         n_staked = get_n_staked()
         n_total = get_n_total()
-        df_all = context.deserialize(r.get('gmx-staked'))
-        df_new = get_staked_recent(df_all, n_total)
-        if df_all.index[-1] == df_new.index[-1]:
-            print('No change')
-        else:
-            print('writing...')
-            r.set('gmx-staked', context.serialize(df_new).to_buffer().to_pybytes())
-        print(df_new)
+        for i, timestamp in enumerate(df_100.index):
+            if timestamp in df_all.index:
+                #print('already stored')
+                pass
+            else:
+                print('requires storing')
+                df_all = df_all.append(df_100.iloc[i])
+        
+        df_all['Staked %'] = df_all['cum_value']/n_total*100
+        r.set('gmx-staked', context.serialize(df_all).to_buffer().to_pybytes())
         # overwrite (set) df tp redis
         r.set('n-gmx-staked', n_staked)
         r.set('n-gmx-total', n_total)
     except Exception as e:
         print(e)
-    time.sleep(30)
+    time.sleep(10)
 
 
